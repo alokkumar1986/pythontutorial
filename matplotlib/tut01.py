8af0ac5b4a9067e2bf3e77c158abc3eb5a71c134
ISU_375 <alok.nayak@iserveu.in>
2024-06-08 09:12:36 +0530

    tut01

---
+++ matplotlib/tut01.py
@@ -0,0 +1,11 @@
+# import matplotlib
+# print(matplotlib.__version__)
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([0,1, 2, 5, 6, 8, 9])
+ypoints = np.array([0,2, 1, 8, 6, 7, 0])
+
+plt.plot(xpoints, ypoints)
+plt.show()
+
